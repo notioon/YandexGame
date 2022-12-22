529c15d6e541f108e0b0e5089618bc3cde3ab327
notioon <dimarus2571@gmail.com>
2022-12-22 20:12:11 +0300

    2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import pygame.sprite
+from datetime import datetime
 from objects import *
+from random import randrange
 
 
 ### Global constants ###
@@ -9,8 +12,9 @@ pygame.font.init()
 FPS = 60
 SIZE = WIDTH, HEIGHT = 800, 400
 SPEED = 4
-HIGH_SCORE = 151
+HIGH_SCORE = 0
 K_SCORE = 1
+GRAVITY = 8
 
 
 def main():
@@ -19,41 +23,98 @@ def main():
 
 
 def gameplay():
+    ### Setting Display ###
     pygame.display.set_caption("Dino 🦕")
     screen = pygame.display.set_mode(SIZE)
-    screen.fill(pygame.Color("white"))
+    screen.fill((69, 69, 69))
+    ### Setting Value ###
+    k_spawn = 1
+    running = True
     clock = pygame.time.Clock()
+    current_score = 0
+    world_map = []
+    count_cloud = 0
+    count_cactus = 0
+    speed = SPEED
+    gravity = GRAVITY
+    time_start = datetime.now()
+    ### Creating Groups ###
     ground_sprites = pygame.sprite.Group()
+    cactus_sprites = pygame.sprite.Group()
     player_sprite = pygame.sprite.Group()
+    cloud_sprite = pygame.sprite.Group()
+    ### Creating Objects
     player = Player(player_sprite)
     ground = Ground(ground_sprites)
+    Cloud(cloud_sprite)
+    Cactus(cactus_sprites)
+    ### Drawing Groups ###
     ground_sprites.draw(screen)
     player_sprite.draw(screen)
-    current_score = 0
-    while True:
-        screen.fill(pygame.Color('white'))
+    cloud_sprite.draw(screen)
+    cactus_sprites.draw(screen)
+    ### Loop ###
+    while running:
+        screen.fill((69, 69, 69))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    if not player.isJumping and not player.isJumping is None:
+                    if not player.isJumping and not player.isJumping is None and not player.isDucking:
                         player.isJumping = True
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_DOWN and not player.isJumping:
                     if not player.isDucking:
                         player.isDucking = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_DOWN:
                     player.isDucking = False
+
+        count_cloud += 1
+        count_cactus += k_spawn * 1
+        ### Spawn Clouds
+        if randrange(0, 150) == 100 and count_cloud > 100:
+            Cloud(cloud_sprite)
+            count_cloud = 0
+        ### Spawn Cactuses
+        if randrange(0, 100) == 50 and count_cactus > 100:
+            Cactus(cactus_sprites)
+            count_cactus = 0
+            k_spawn += 0.1
+
+
         current_score += 10/60
-        speed = current_score // 100 + SPEED
-        ground.update(speed)
-        player.update()
+        ### Drawing Score
         score(screen, max(HIGH_SCORE, int(str(current_score).split('.')[0])), str(current_score).split('.')[0])
+
+        if (datetime.now() - time_start).seconds == 10:
+            speed += 0.5
+            gravity += 1
+            time_start = datetime.now()
+        ### Checking collision ###
+        for cactus in cactus_sprites:
+            if pygame.sprite.collide_mask(player, cactus):
+                player.isDead = True
+                running = False
+        ### Updating ###
+        cactus_sprites.update(speed)
+        ground.update(speed)
+        player.update(gravity)
+        cloud_sprite.update(speed * 0.5)
+        ### Drawing ###
         ground_sprites.draw(screen)
         player_sprite.draw(screen)
+        cloud_sprite.draw(screen)
+        cactus_sprites.draw(screen)
         pygame.display.flip()
+
         clock.tick(FPS)
+
+    gameover()
+
+
+def gameover():
+    pass
 
 
 def score(screen, max, score):
--- objects.py
+++ objects.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+from random import randrange, choice
 
 
 def load_image(name):
@@ -14,25 +15,50 @@ def load_image(name):
 
 
 class Player(pygame.sprite.Sprite):
-    image_usualy = load_image("player.png")
-    image_ducking = load_image("player_ducking.png")
-
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = self.image_usualy
+        self.frames = []
+        self.frames_ducking = []
+        self.cut_sheet(load_image("player_sprite.png"), 5, 1, self.frames)
+        self.cut_sheet(load_image("player_ducking_sprite.png"), 2, 1, self.frames_ducking)
+        del self.frames[1], self.frames[0]
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
         self.isJumping = False
+        self.gravity = 0
         self.Jumping_count = 0
         self.isDucking = False
-        self.gravity = 8
+        self.isDead = False
         self.rect = self.image.get_rect()
-        self.rect.x = 50
-        self.rect.y = 240
+        self.rect.x, self.rect.y = 50, 240
+        self.count_frame = 0
 
-    def update(self):
-        if self.isJumping:  # Прыжок
-            if self.rect.y > 130:
+    def cut_sheet(self, sheet, columns, rows, lst):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                lst.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self, gravity):
+        self.count_frame += 1
+        ### Animation ###
+        if self.isDead:
+            self.image = self.frames[-1]
+        if self.count_frame % 7 == 0:
+            if self.cur_frame + 1 == 2:
+                self.cur_frame = 0
+            else:
+                self.cur_frame += 1
+        self.image = self.frames[self.cur_frame]
+
+        ### Jumping ###
+        if self.isJumping:
+            if self.rect.y > 140:
                 self.rect.y -= self.gravity
-            elif self.Jumping_count < 7:
+            elif self.Jumping_count < 15:
                 self.Jumping_count += 1
             else:
                 self.isJumping = None
@@ -42,10 +68,14 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.Jumping_count = 0
                 self.isJumping = False
-        if self.isDucking:
-            self.image = self.image_ducking
         else:
-            self.image = self.image_usualy
+            self.gravity = gravity
+
+        ### Ducking ###
+        if self.isDucking:
+            self.image = self.frames_ducking[self.cur_frame]
+        else:
+            self.image = self.frames[self.cur_frame]
 
 
 class Ground(pygame.sprite.Sprite):
@@ -66,3 +96,49 @@ class Scoreboard(pygame.sprite.Sprite):
 
     def __init__(self, *group):
         super().__init__(*group)
+
+
+class Cloud(pygame.sprite.Sprite):
+    image = load_image("cloud.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.rect = self.image.get_rect()
+        self.rect.x = 800
+        self.rect.y = randrange(20, 100)
+        self.speed = 2
+
+    def update(self, speed):
+        self.rect.x -= speed
+
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super().__init__(*group)
+        rnd = choice([0, 1, 2, 3, 4])
+        if rnd == 0:
+            self.image = self.cut_sheet(load_image("cactus_big.png"), 6, 1)[0]
+        elif rnd == 1:
+            self.image = self.cut_sheet(load_image("cactus_big.png"), 6, 1)[0]
+        elif rnd == 2:
+            self.image = self.cut_sheet(load_image("cactus_big.png"), 3, 1)[0]
+        elif rnd == 3:
+            self.image = self.cut_sheet(load_image("cactus_big.png"), 2, 1)[0]
+        elif rnd == 4:
+            self.image = load_image("cactus_3.png")
+
+        self.rect = self.image.get_rect()
+        self.rect.y = 260
+        self.rect.x = 800
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                return [sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size))]
+
+    def update(self, speed):
+        self.rect.x -= speed
