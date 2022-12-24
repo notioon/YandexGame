30825859d07322b3dbde39b1fba2f4c76ba8836f
notioon <dimarus2571@gmail.com>
2022-12-24 23:22:07 +0300

    Optimize + Added Coin Spawn & GameOver

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ def gameplay():
     screen = pygame.display.set_mode(SIZE)
     screen.fill((69, 69, 69))
     ### Setting Value ###
-    k_spawn = 1
     running = True
+    k_spawn = 1
     clock = pygame.time.Clock()
     current_score = 0
-    world_map = []
     count_cloud = 0
     count_cactus = 0
+    count_coins = 0
     speed = SPEED
     gravity = GRAVITY
     time_start = datetime.now()
@@ -43,6 +43,7 @@ def gameplay():
     cactus_sprites = pygame.sprite.Group()
     player_sprite = pygame.sprite.Group()
     cloud_sprite = pygame.sprite.Group()
+    coins_sprite = pygame.sprite.Group()
     ### Creating Objects
     player = Player(player_sprite)
     ground = Ground(ground_sprites)
@@ -72,20 +73,26 @@ def gameplay():
 
         count_cloud += 1
         count_cactus += k_spawn * 1
-        ### Spawn Clouds
+        ### Spawn Clouds ###
         if randrange(0, 150) == 100 and count_cloud > 100:
             Cloud(cloud_sprite)
             count_cloud = 0
-        ### Spawn Cactuses
+        ### Spawn Cactuses ###
         if randrange(0, 100) == 50 and count_cactus > 100:
             Cactus(cactus_sprites)
             count_cactus = 0
             k_spawn += 0.1
+        ### Spawn Coins ###
+        if randrange(0, 150) == 100:
+            coin = Coin(coins_sprite)
+            for cactus in cactus_sprites:
+                if pygame.sprite.collide_mask(coin, cactus):
+                    coin.rect.y = 200
 
 
         current_score += 10/60
         ### Drawing Score
-        score(screen, max(HIGH_SCORE, int(str(current_score).split('.')[0])), str(current_score).split('.')[0])
+        score(screen, max(HIGH_SCORE, int(str(current_score).split('.')[0])), str(current_score).split('.')[0], count_coins)
 
         if (datetime.now() - time_start).seconds == 10:
             speed += 0.5
@@ -94,34 +101,58 @@ def gameplay():
         ### Checking collision ###
         for cactus in cactus_sprites:
             if pygame.sprite.collide_mask(player, cactus):
-                player.isDead = True
+                player_sprite.update(gravity, True)
+                player_sprite.draw(screen)
+                pygame.display.flip()
                 running = False
+
+        for coin in coins_sprite:
+            if pygame.sprite.collide_mask(player, coin):
+                count_coins += 1
+                coin.isVisible = False
+
         ### Updating ###
         cactus_sprites.update(speed)
         ground.update(speed)
         player.update(gravity)
         cloud_sprite.update(speed * 0.5)
+        coins_sprite.update(speed)
         ### Drawing ###
         ground_sprites.draw(screen)
         player_sprite.draw(screen)
         cloud_sprite.draw(screen)
         cactus_sprites.draw(screen)
+        coins_sprite.draw(screen)
         pygame.display.flip()
 
         clock.tick(FPS)
 
-    gameover()
+    gameover(screen)
 
 
-def gameover():
-    pass
+def gameover(screen):
+    buttons = pygame.sprite.Group()
+    replay_button = ReplayButton(buttons)
+    main_menu_button = MainMenuButton(buttons)
+    GameOver(buttons)
+    buttons.draw(screen)
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if replay_button.rect.collidepoint(event.pos):
+                    gameplay()
+                elif main_menu_button.rect.collidepoint(event.pos):
+                    pass  # main menu
 
 
-def score(screen, max, score):
+def score(screen, max, score, coins):
     font = pygame.font.Font("Font.ttf", 13)
-    text = font.render("HI:" + str(max) + " " + str(score), True, (pygame.Color("black")))
+    text = font.render("HI:" + str(max) + " " + str(score) + " M " + str(coins), True, (pygame.Color("black")))
     text_print = text.get_rect()
-    text_print.center = 690, 40
+    text_print.center = 680, 40
     screen.blit(text, text_print)
 
 
@@ -135,8 +166,7 @@ def start_window():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 return
         pygame.display.flip()
 
--- objects.py
+++ objects.py
@@ -13,45 +13,37 @@ def load_image(name):
     return image
 
 
+def cut_sheet(sheet, columns, rows):
+    rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
+    return [sheet.subsurface(pygame.Rect((rect.w * i, rect.h * j), rect.size)) for j in range(rows) for i in
+            range(columns)]
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, *group):
         super().__init__(*group)
-        self.frames = []
-        self.frames_ducking = []
-        self.cut_sheet(load_image("player_sprite.png"), 5, 1, self.frames)
-        self.cut_sheet(load_image("player_ducking_sprite.png"), 2, 1, self.frames_ducking)
-        del self.frames[1], self.frames[0]
+        self.frames = cut_sheet(load_image("player_sprite.png"), 3, 1)
+        self.frames_ducking = cut_sheet(load_image("player_ducking_sprite.png"), 2, 1)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.isJumping = False
         self.gravity = 0
         self.Jumping_count = 0
         self.isDucking = False
-        self.isDead = False
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 50, 240
         self.count_frame = 0
 
-    def cut_sheet(self, sheet, columns, rows, lst):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                lst.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self, gravity):
+    def update(self, gravity, isDead=None):
         self.count_frame += 1
         ### Animation ###
-        if self.isDead:
-            self.image = self.frames[-1]
         if self.count_frame % 7 == 0:
             if self.cur_frame + 1 == 2:
                 self.cur_frame = 0
             else:
                 self.cur_frame += 1
+        if isDead:
+            self.cur_frame = -1
         self.image = self.frames[self.cur_frame]
 
         ### Jumping ###
@@ -92,12 +84,6 @@ class Ground(pygame.sprite.Sprite):
         self.rect.x -= speed
 
 
-class Scoreboard(pygame.sprite.Sprite):
-
-    def __init__(self, *group):
-        super().__init__(*group)
-
-
 class Cloud(pygame.sprite.Sprite):
     image = load_image("cloud.png")
 
@@ -110,6 +96,8 @@ class Cloud(pygame.sprite.Sprite):
 
     def update(self, speed):
         self.rect.x -= speed
+        if self.rect.x < -50:
+            self.kill()
 
 
 class Cactus(pygame.sprite.Sprite):
@@ -117,13 +105,13 @@ class Cactus(pygame.sprite.Sprite):
         super().__init__(*group)
         rnd = choice([0, 1, 2, 3, 4])
         if rnd == 0:
-            self.image = self.cut_sheet(load_image("cactus_big.png"), 6, 1)[0]
+            self.image = cut_sheet(load_image("cactus_big.png"), 6, 1)[0]
         elif rnd == 1:
-            self.image = self.cut_sheet(load_image("cactus_big.png"), 6, 1)[0]
+            self.image = cut_sheet(load_image("cactus_big.png"), 6, 1)[0]
         elif rnd == 2:
-            self.image = self.cut_sheet(load_image("cactus_big.png"), 3, 1)[0]
+            self.image = cut_sheet(load_image("cactus_big.png"), 3, 1)[0]
         elif rnd == 3:
-            self.image = self.cut_sheet(load_image("cactus_big.png"), 2, 1)[0]
+            self.image = cut_sheet(load_image("cactus_big.png"), 2, 1)[0]
         elif rnd == 4:
             self.image = load_image("cactus_3.png")
 
@@ -131,14 +119,61 @@ class Cactus(pygame.sprite.Sprite):
         self.rect.y = 260
         self.rect.x = 800
 
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                return [sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size))]
-
     def update(self, speed):
         self.rect.x -= speed
+        if self.rect.x < -50:
+            self.kill()
+
+
+class ReplayButton(pygame.sprite.Sprite):
+    image = load_image("replay_button.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 381, 250
+
+
+class GameOver(pygame.sprite.Sprite):
+    image = load_image("game_over.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 260, 130
+
+
+class MainMenuButton(pygame.sprite.Sprite):
+    image = load_image("main_menu.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 385, 300
+
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.isVisible = True
+        self.frames = cut_sheet(load_image("coins.png"), 9, 1)
+        self.image = self.frames[0]
+        self.count = 0
+        self.frame_count = 0
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 800, 253
+
+    def update(self, speed):
+        if not self.isVisible:
+            self.kill()
+        else:
+            self.count += 1
+            if self.count % 7 == 0:
+                if self.frame_count + 1 == 6:
+                    self.frame_count = 0
+                else:
+                    self.frame_count += 1
+            self.image = self.frames[self.frame_count]
+            self.rect.x -= speed
+        if self.rect.x < -50:
+            self.kill()
